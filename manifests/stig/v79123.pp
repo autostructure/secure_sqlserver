6c416d9b356f49045979c05d19a3ad8c9a344c89
Jeffrey Frelick <jeff@autostructure.io>
2018-06-11 00:52:45 -0400

    debugging

--- manifests/stig/v79123.pp
+++ manifests/stig/v79123.pp
@@ -9,7 +9,7 @@ class secure_sqlserver::stig::v79123 (
   $fqdn = $facts['fqdn']
   $port = 1433
   $svc_acct = "${facts['domain']}\${facts['id']}"
-  secure_sqlserver::log { $svc_acct: }
+  secure_sqlserver::log { "service account = ${svc_acct}": }
   #$svc_acct = 'WIN-OKVLNTQGMS4\Administrator'
   #$svc_acct = 'jeff-win-2012-sqlserver\Administrator'
   $cmd_setspn_fqdn = "setspn -S MSSQLSvc/${fqdn} ${svc_acct}"
