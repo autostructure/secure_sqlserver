96bcea73d38bc7530b0b73f43685d94e182a57cd
Jeffrey Frelick <jeff@autostructure.io>
2018-06-13 15:19:10 -0400

    debugging version check

--- manifests/version_check.pp
+++ manifests/version_check.pp
@@ -4,6 +4,11 @@
 class secure_sqlserver::version_check ()
 {
 
+  notify { 'ver chk msg':
+    message  => $::secure_sqlserver::logon::version,
+    loglevel => 'warning',
+  }
+
   # if $::secure_sqlserver::logon::version != 'SQL_2016' {
   if $::secure_sqlserver::logon::version != 'SQL_2016' {
     fail("Unsupported MS SQL Server version detected, found ${::secure_sqlserver::logon::version} instead of SQL_2016.")
