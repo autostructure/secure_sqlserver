c4d85b73a27e10153f6b03ce581a7dea8ebd43ae
Jeffrey Frelick <jeff@autostructure.io>
2018-06-13 16:45:35 -0400

    debugging log.pp

--- manifests/log.pp
+++ manifests/log.pp
@@ -7,13 +7,19 @@ define secure_sqlserver::log ()
   # Boolean $enabled = hiera('secure_sqlserver::log::enabled', true),
   # Enum['alert', 'crit', 'debug', 'emerg', 'err', 'info', 'notice', 'warning'] $threatlevel = 'warning',
 
-  Enum['alert', 'crit', 'debug', 'emerg', 'err', 'info', 'notice', 'warning'] $threatlevel = hiera('secure_sqlserver::log::threatlevel', 'warning')
-  Boolean $enabled = hiera('secure_sqlserver::log::enabled', true)
+  # $loglevel = hiera('secure_sqlserver::log::loglevel', 'warning')
+  # Boolean $enabled = hiera('secure_sqlserver::log::enabled', true)
+  $enabled = true
+  $loglevel = 'warning'
+
+  # if $loglevel !=~ Enum['alert', 'crit', 'debug', 'emerg', 'err', 'info', 'notice', 'warning'] {
+  #  fail("Invalid loglevel specified (${loglevel}).\nValid loglevel values include: 'alert', 'crit', 'debug', 'emerg', 'err', 'info', 'notice', 'warning'.")
+  # }
 
   if $enabled {
 
     # puppetserver.log
-    case $threatlevel {
+    case $loglevel {
       'alert':   { alert("${facts['fqdn']}: ${title}") }    # always visible
       'crit':    { crit("${facts['fqdn']}: ${title}") }     # always visible
       'debug':   { debug("${facts['fqdn']}: ${title}") }    # visible only with -d or --debug
@@ -29,7 +35,7 @@ define secure_sqlserver::log ()
     notify { "puppetagentlogger_${title}":
       withpath => false,
       message  => $title,
-      loglevel => $threatlevel,
+      loglevel => $loglevel,
     }
 
   }
