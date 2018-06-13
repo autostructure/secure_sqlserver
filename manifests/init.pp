3e402cebcb518c190cb73ffaa8e117995a64a766
Jeffrey Frelick <jeff@autostructure.io>
2018-06-13 15:52:57 -0400

    tests ordering

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,9 +9,14 @@ class secure_sqlserver
     provider => 'gem',
   }
 
-  class { '::secure_sqlserver::logon': }
-    -> class { '::secure_sqlserver::version_check': }
-  #  -> class { '::secure_sqlserver::secure_instance': }
-  #  -> class { '::secure_sqlserver::secure_database': }
+  Class['secure_sqlserver::logon']
+    -> Class['secure_sqlserver::version_check']
+    -> Class['secure_sqlserver::secure_instance']
+    -> Class['secure_sqlserver::secure_database']
+
+  # class { '::secure_sqlserver::logon': }
+  #   -> class { '::secure_sqlserver::version_check': }
+  #   -> class { '::secure_sqlserver::secure_instance': }
+  #   -> class { '::secure_sqlserver::secure_database': }
 
 }
