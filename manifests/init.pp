eb58cbbcfb8cd192b58430ab5f8989f7eae9e8f1
Jeffrey Frelick <jeff@autostructure.io>
2018-06-13 16:24:12 -0400

    test

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,11 +11,13 @@ class secure_sqlserver
 
   include ::secure_sqlserver::logon
   include ::secure_sqlserver::version_check
+  include ::secure_sqlserver::secure_instance
   include ::secure_sqlserver::secure_database
 
   Package['tiny_tds']
     -> Class['::secure_sqlserver::logon']
     -> Class['::secure_sqlserver::version_check']
+    -> Class['::secure_sqlserver::secure_instance']
     -> Class['::secure_sqlserver::secure_database']
 
   #Package['tiny_tds']
