4abf701d86df3b9fb7b6ffe3f93c366a3d603d34
Jeffrey Frelick <jeff@autostructure.io>
2018-06-13 16:00:57 -0400

    testing

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,14 +9,15 @@ class secure_sqlserver
     provider => 'gem',
   }
 
-  Class['secure_sqlserver::logon']
-    -> Class['secure_sqlserver::version_check']
-    -> Class['secure_sqlserver::secure_instance']
-    -> Class['secure_sqlserver::secure_database']
+  include ::secure_sqlserver::logon
+  include ::secure_sqlserver::version_check
+  include ::secure_sqlserver::secure_instance
+  include ::secure_sqlserver::secure_database
 
-  # class { '::secure_sqlserver::logon': }
-  #   -> class { '::secure_sqlserver::version_check': }
-  #   -> class { '::secure_sqlserver::secure_instance': }
-  #   -> class { '::secure_sqlserver::secure_database': }
+  Package['tiny_tds']
+    -> Class['::secure_sqlserver::logon']
+    -> Class['::secure_sqlserver::version_check']
+    -> Class['::secure_sqlserver::secure_instance']
+    -> Class['::secure_sqlserver::secure_database']
 
 }
