534a3fdc95969fca755b7a2d8e6f9cec4f6ee709
Jeffrey Frelick <jeff@autostructure.io>
2018-06-13 16:09:45 -0400

    rename file

--- manifests/secure_database999.pp
+++ manifests/secure_database999.pp
@@ -1,6 +1,6 @@
 # This class calls all classes that secure a MS SQL Server 2016 database.
 #
-class secure_sqlserver::secure_database2 ()
+class secure_sqlserver::secure_database ()
 {
   # database STIGs...
   # class { '::secure_sqlserver::stig::v79061': }
