4305dc9b925ad9d7cc5fe2698cae7fc89dc0d809
Jeffrey Frelick <jeff@autostructure.io>
2018-06-15 16:21:22 -0400

    test

--- lib/facter/sqlserver_roles_assigned_to_nt_authority_system.rb
+++ lib/facter/sqlserver_roles_assigned_to_nt_authority_system.rb
@@ -34,7 +34,7 @@ Facter.add('sqlserver_roles_assigned_to_nt_authority_system') do
 
     begin
 
-      config = [admin_login_type: 'WINDOWS_LOGIN', instance_name: 'MSSQLSERVER', database: 'MSSQLSERVER', admin_user: '', admin_pass: '', host: 'localhost', database: 'MSSQLSERVER']# lint:ignore:140chars
+      config = { admin_login_type: 'WINDOWS_LOGIN', instance_name: 'MSSQLSERVER', database: 'MSSQLSERVER', admin_user: '', admin_pass: '', host: 'localhost' } # lint:ignore:140chars
       connect = PuppetX::Sqlserver::SqlConnection.new
       results = connect.open_and_run_command(sql, config)
 
