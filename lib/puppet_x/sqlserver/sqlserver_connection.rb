902a5dbbee06959d9d0e55fc9e4f05bdb037d9b5
Jeffrey Frelick <jeff@autostructure.io>
2018-06-18 14:42:30 -0400

    test

--- lib/puppet_x/sqlserver/sqlserver_connection.rb
+++ lib/puppet_x/sqlserver/sqlserver_connection.rb
@@ -10,37 +10,50 @@ require File.expand_path(File.join(File.dirname(__FILE__), '../..', 'puppet_x/sq
 module PuppetX
   module Sqlserver
 
+    # Extend puppet SqlConnection class...
     class SqlServerConnection < SqlConnection
-
-      class ResultSet
-
-        attr_reader :exitstatus, :error_message
-
-        def getResults()
-          Puppet.debug connnection.methods
-          return 'test'
+      def results(query, config)
+        begin
+          open(config)
+          results = connection.Execute(sql, nil, nil)
+        rescue win32_exception => e
+          Puppet.debug "SqlServerConnection error: #{e.message}"
+          return ResultOutput.new(true, e.message, @connection)
+        ensure
+          close
         end
+        results
+      end
+    end
 
-        def initialize(has_errors, error_message, connection)
-          @exitstatus = has_errors ? 1 : 0
+    # Internal class
+    class ResultSet
+      attr_reader :exitstatus, :error_message
 
-          @error_message = extract_messages(connection) || error_message
-        end
-
-        def extract_messages(connection)
-          return nil if connection.nil? || connection.Errors.count == 0
-
-          error_count = connection.Errors.count - 1
-
-          ((0..error_count).map { |i| connection.Errors(i).Description.to_s}).join("\n")
-        end
-
-        def has_errors
-          @exitstatus != 0
-        end
+      def results()
+        Puppet.debug connnection.methods
+        return 'test'
       end
 
+      def initialize(has_errors, error_message, connection)
+        @exitstatus = has_errors ? 1 : 0
+        @error_message = extract_messages(connection) || error_message
+      end
+
+      def extract_messages(connection)
+        return nil if connection.nil? || connection.Errors.count.zero?
+
+        error_count = connection.Errors.count - 1
+
+        ((0..error_count).map { |i| connection.Errors(i).Description.to_s}).join("\n")
+      end
+
+      def has_errors
+        @exitstatus != 0
+      end
     end
+
+
 
   end
 end
