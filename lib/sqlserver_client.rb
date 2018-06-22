e4e0a8f5e0c2d2ea24a9790b9c9439160b677623
Jeffrey Frelick <jeff@autostructure.io>
2018-06-22 11:37:27 -0400

    test

--- lib/sqlserver_client.rb
+++ lib/sqlserver_client.rb
@@ -141,7 +141,8 @@ class SqlServerClient
       # Move to the first record/row, if any exist
       recordset.MoveFirst
       rows = recordset.GetRows
-      rows.each { |datum| @data << datum }
+      @data = rows.flatten
+      #ows.each { |datum| @data << datum }
       # An ADO Recordset's GetRows method returns an array of columns,
       # so we'll use the transpose method to convert it to an array of rows
     rescue
