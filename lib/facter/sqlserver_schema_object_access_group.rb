cf79ff8dd70b016eddacee292d7177771614b6f2
Jeffrey Frelick <jeff@autostructure.io>
2018-06-28 13:04:56 -0400

    v79137

--- lib/facter/sqlserver_schema_object_access_group.rb
+++ lib/facter/sqlserver_schema_object_access_group.rb
@@ -3,30 +3,10 @@
 # SQL Server must be configured to generate audit records for DoD-defined
 # auditable events within all DBMS/database components.
 #
-# DoD has defined the list of events for which SQL Server will provide an
-# audit record generation capability as the following:
-#
-# (i) Successful and unsuccessful attempts to access, modify, or delete privileges,
-#     security objects, security levels, or categories of information
-#     (e.g., classification levels);
-# (ii) Access actions, such as successful and unsuccessful logon attempts,
-#      privileged activities, or other system-level access, starting and ending
-#      time for user access to the system, concurrent logons from different workstations,
-#      successful and unsuccessful accesses to objects, all program initiations,
-#      and all direct access to the information system; and
-# (iii) All account creation, modification, disabling, and termination actions.
-#
-# In a high security environment, the Windows Security log is the appropriate
-# location to write events that record object access. Other audit locations
-# are supported but are more subject to tampering.
-# Source:
-# https://docs.microsoft.com/en-us/sql/relational-databases/security/auditing/write-sql-server-audit-events-to-the-security-log?view=sql-server-2017
-#
 # Dependencies:
-# v79133
 # v79137
 #
-# @return   An array of hashes representing audit event rule records.
+# @return   An array of strings representing the SCHEMA_OBJECT_ACCESS_GROUP records.
 # @example
 #
 require 'sqlserver_client'
@@ -35,13 +15,20 @@ Facter.add('sqlserver_schema_object_access_group') do
   confine operatingsystem: :windows
   setcode do
 
-    sql = "SELECT name AS 'audit_name', status_desc AS 'audit_status', audit_file_path AS 'current_audit_file' FROM sys.dm_server_audit_status"
+    sql = "SELECT a.name AS 'AuditName',
+       s.name AS 'SpecName',
+       d.audit_action_name AS 'ActionName',
+       d.audited_result AS 'Result'
+  FROM sys.server_audit_specifications s
+  JOIN sys.server_audits a ON s.audit_guid = a.audit_guid
+  JOIN sys.server_audit_specification_details d ON s.server_specification_id = d.server_specification_id
+ WHERE a.is_state_enabled = 1 AND d.audit_action_name = 'SCHEMA_OBJECT_ACCESS_GROUP'"
 
     Puppet.debug "sqlserver_v79133_auditable_events.rb sql...\n#{sql}"
 
     client = SqlServerClient.new
     client.open
-    client.column(sql)
+    client.hasharray(sql)
     resultset = client.data
     client.close unless client.nil? || client.closed?
     resultset
