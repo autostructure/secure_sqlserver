6fbf25f5cf3aa8cae5abd6ae6bd54de4be8eb809
Jeffrey Frelick <jeff@autostructure.io>
2018-07-03 11:36:25 -0400

    new fact 4 v79165

---
+++ lib/facter/sqlserver_v79165_file_changes.rb
@@ -0,0 +1,21 @@
+# sqlserver_v79165_file_changes.rb
+#
+# @return
+# @example
+#
+# Get-FileHash -Path "c:\Windows\Temp\* -Algorithm MD5 | ConvertTo-Json | Out-File "c:\puppet.secure-sqlserver.filehashes.json"
+# Get-FileHash -Path "c:\Windows\Temp\* -Algorithm MD5 | ConvertTo-Json | Out-File "c:\puppet.secure-sqlserver.temp.json"
+# Compare-Object $(Get-Content "c:\puppet.secure-sqlserver.filehashes.json") $(Get-Content "c:\puppet.secure-sqlserver.temp.json")
+#
+Facter.add('sqlserver_v79165_file_changes') do
+  confine operatingsystem: :windows
+  setcode do
+    cmd1 = "if (-NOT (Test-Path \"c:\puppet.secure-sqlserver.filehash-baseline.json\")) { Get-FileHash -Path \"c:\Windows\Temp\*\" -Algorithm MD5 | ConvertTo-Json | Out-File \"c:\\puppet.secure-sqlserver.filehash-baseline.json\" }"# lint:ignore:140chars
+    cmd2 = "Get-FileHash -Path \"c:\Windows\Temp\*\" -Algorithm MD5 | ConvertTo-Json | Out-File \"c:\\puppet.secure-sqlserver.filehash-tempfile.json\""# lint:ignore:140chars
+    cmd3 = "Compare-Object $(Get-Content \"c:\\puppet.secure-sqlserver.filehashes.json\") $(Get-Content \"c:\\puppet.secure-sqlserver.temp.json\")"# lint:ignore:140chars
+    Facter::Core::Execution.exec("powershell.exe -Command \"#{cmd1}\"")
+    Facter::Core::Execution.exec("powershell.exe -Command \"#{cmd2}\"")
+    result = Facter::Core::Execution.exec("powershell.exe -Command \"#{cmd3}\"")
+    result
+  end
+end
--- lib/facter/sqlserver_whoami_sid.rb
+++ lib/facter/sqlserver_whoami_sid.rb
@@ -1,12 +1,10 @@
 # sqlserver_whoami_sid.rb
-# 
+#
 # Some registry resources require a SID.
 #
 # @return   The SID of the current user.
 # @example  S-1-5-21-804-36083-3409395816-4202414035-500
 #
-require 'sqlserver_client'
-
 Facter.add('sqlserver_whoami_sid') do
   confine operatingsystem: :windows
   setcode do
