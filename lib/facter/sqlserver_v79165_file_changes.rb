ee9a84cedf018a13ff5aa60833868d73b7a3466a
Jeffrey Frelick <jeff@autostructure.io>
2018-07-03 14:18:45 -0400

    fixing fact

--- lib/facter/sqlserver_v79165_file_changes.rb
+++ lib/facter/sqlserver_v79165_file_changes.rb
@@ -10,9 +10,9 @@
 Facter.add('sqlserver_v79165_file_changes') do
   confine operatingsystem: :windows
   setcode do
-    cmd1 = "if (-NOT (Test-Path \"c:\puppet.secure-sqlserver.filehash-baseline.json\")) { Get-FileHash -Path \"c:\\Windows\\Temp\\*\" -Algorithm MD5 | ConvertTo-Json | Out-File \"c:\\puppet.secure-sqlserver.filehash-baseline.json\" }"# lint:ignore:140chars
+    cmd1 = "if (-NOT (Test-Path \"c:\\puppet.secure-sqlserver.filehash-baseline.json\")) { Get-FileHash -Path \"c:\\Windows\\Temp\\*\" -Algorithm MD5 | ConvertTo-Json | Out-File \"c:\\puppet.secure-sqlserver.filehash-baseline.json\" }"# lint:ignore:140chars
     cmd2 = "Get-FileHash -Path \"c:\\Windows\\Temp\\*\" -Algorithm MD5 | ConvertTo-Json | Out-File \"c:\\puppet.secure-sqlserver.filehash-tempfile.json\""# lint:ignore:140chars
-    cmd3 = "Compare-Object $(Get-Content \"c:\\puppet.secure-sqlserver.filehashes.json\") $(Get-Content \"c:\\puppet.secure-sqlserver.temp.json\")"# lint:ignore:140chars
+    cmd3 = "Compare-Object $(Get-Content \"c:\\puppet.secure-sqlserver.filehash-baseline.json\") $(Get-Content \"c:\\puppet.secure-sqlserver.filehash-tempfile.json\")"# lint:ignore:140chars
     Facter::Core::Execution.exec("powershell.exe -Command \"#{cmd1}\"")
     Facter::Core::Execution.exec("powershell.exe -Command \"#{cmd2}\"")
     result = Facter::Core::Execution.exec("powershell.exe -Command \"#{cmd3}\"")
