f9114e761968869cff3cbf8218f4f2611e7dc3c9
Jeffrey Frelick <jeff@autostructure.io>
2018-07-24 13:57:31 -0400

    v79177

---
+++ manifests/stig/v79177.pp
@@ -0,0 +1,25 @@
+# v79171.pp
+# Default demonstration and sample databases, database objects, and applications must be removed.
+#
+# Remove demonstration databases:
+# 1. pubs
+# 2. Northwinds
+# 3. AdventureWorks
+# 4. WorldwideImporters
+#
+class secure_sqlserver::stig::v79171 (
+  Boolean $enforced = false,
+  String  $instance = 'MSSQLSERVER',
+) {
+  if $enforced {
+
+    $sql = 'DROP DATABASE IF EXISTS pubs, Northwinds, AdventureWorks, WorldwideImporters'
+
+    sqlserver_tsql{ 'v79171-drop-demo-databases':
+      instance => $instance,
+      command  => $sql,
+      require  => Sqlserver::Config[$instance],
+    }
+
+  }
+}
