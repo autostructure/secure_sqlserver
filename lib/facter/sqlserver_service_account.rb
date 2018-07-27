bf0f7eee12993f04b1a8ee2b260ea16d8abe6b56
Jeffrey Frelick <jeff@autostructure.io>
2018-07-27 15:14:20 -0400

    service account fact debugging

--- lib/facter/sqlserver_service_account.rb
+++ lib/facter/sqlserver_service_account.rb
@@ -7,7 +7,8 @@ Facter.add('sqlserver_service_account') do
   confine operatingsystem: :windows
   setcode do
 
-    Hiera.lookup('profile::windows_sqlserver::svc_acct','Service account not found.')
-
+    #Hiera.lookup('profile::windows_sqlserver::svc_acct')
+    Hiera.lookup('secure_sqlserver::stig::v79119::enforced')
+    
   end
 end
