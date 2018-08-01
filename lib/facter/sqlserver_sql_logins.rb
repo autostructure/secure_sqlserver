22903d0cf75b9812d234978d8c25e6dd27cf75fe
Jeffrey Frelick <jeff@autostructure.io>
2018-08-01 16:16:51 -0400

    edit 2 fact comment

--- lib/facter/sqlserver_sql_logins.rb
+++ lib/facter/sqlserver_sql_logins.rb
@@ -1,19 +1,21 @@
 # sqlserver_sql_logins.rb
 #
-# Return users using SQL LOGIN, not WINDOWS LOGIN.
+# Return users using SQL Server authentication, not Windows authentication.
 #
 # type_desc
 # ---------
 # CERTIFICATE_MAPPED_USER
-# SERVER_ROLE
+# ASYMMETRIC_KEY_MAPPED_USER
+# APPLICATION_ROLE
 # DATABASE_ROLE
+# EXTERNAL_USER
 # SQL_USER
 # WINDOWS_USER
-# GROUP
+# WINDOWS_GROUP
+# EXTERNAL_GROUPS
 #
 # @return   An array of strings representing accounts using SQL Server authentication.
 # @example  ["public","dbo","guest"]
-#
 #
 require 'sqlserver_client'
 
