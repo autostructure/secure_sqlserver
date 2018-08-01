fbdd31dcdf8f8cf8f651638716258f56992272b2
Jeffrey Frelick <jeff@autostructure.io>
2018-08-01 15:38:57 -0400

    test fact

--- lib/facter/sqlserver_sql_logins.rb
+++ lib/facter/sqlserver_sql_logins.rb
@@ -2,13 +2,14 @@
 #
 # Return users using SQL LOGIN, not WINDOWS LOGIN.
 #
-# Type Description
-# ---- ------------------------
-# C    CERTIFICATE_MAPPED_LOGIN
-# R    SERVER_ROLE
-# S    SQL_LOGIN
-# U    WINDOWS_LOGIN
-# G    GROUP
+# type_desc
+# ---------
+# CERTIFICATE_MAPPED_USER
+# SERVER_ROLE
+# DATABASE_ROLE
+# SQL_USER
+# WINDOWS_USER
+# GROUP
 #
 # @return   An array of strings representing accounts using SQL Server authentication.
 # @example  ['user1','user2']
@@ -20,7 +21,8 @@ Facter.add('sqlserver_sql_logins.rb') do
   confine operatingsystem: :windows
   setcode do
 
-    sql = "SELECT name FROM sys.database_principals WHERE type_desc = 'SQL_USER' AND authentication_type_desc = 'DATABASE';"
+    #sql = "SELECT name FROM sys.database_principals WHERE type_desc = 'SQL_USER' AND authentication_type_desc = 'DATABASE';"
+    sql = "SELECT name FROM sys.database_principals"
 
     Puppet.debug "sqlserver_sql_logins.rb sql...\n#{sql}"
 
