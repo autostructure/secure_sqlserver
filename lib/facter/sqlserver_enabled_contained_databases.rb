0412f0306330261fdc1af489ae01680c8efdaeb8
Frelick <12001003596630@FEDIDCARD.GOV>
2018-08-07 17:37:14 -0400

    fixed sqlserver_enabled_contained_databases fact

--- lib/facter/sqlserver_enabled_contained_databases.rb
+++ lib/facter/sqlserver_enabled_contained_databases.rb
@@ -20,8 +20,10 @@ Facter.add('sqlserver_enabled_contained_databases') do
     client.hasharray(sql)
     resultset = client.data
     client.close unless client.nil? || client.closed?
-    Puppet.debug "resultset[0]=#{resultset[0]}"
-    Puppet.debug "resultset[0]['config_value']=#{resultset[0]['config_value']}"
+
+    #Puppet.debug "resultset[0]=#{resultset[0]}"
+
     resultset[0]['config_value']==1 ? true : false
+
   end
 end
