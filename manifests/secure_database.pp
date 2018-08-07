73a33030ab8d2c447f52532485f30628462e3b74
Frelick <12001003596630@FEDIDCARD.GOV>
2018-08-07 12:38:17 -0400

    fixing duplicate class declaration in secure_database

--- manifests/secure_database.pp
+++ manifests/secure_database.pp
@@ -5,13 +5,18 @@ define secure_sqlserver::secure_database (
   String[1,128] $database,
 ) {
 
+  $class1 = "${instance}::${database}::secure_sqlserver::stig::v79061"
+  $class2 = "${instance}::${database}::secure_sqlserver::stig::v79071"
+
+  ::secure_sqlserver::log { "secure_database...\n${class1}\n${class2}": }
+
   # Database STIGs...
-  class { "${instance}::${database}::secure_sqlserver::stig::v79061":
+  class { $class1 :
     instance => $instance,
     database => $database,
   }
 
-  class { "${instance}::${database}::secure_sqlserver::stig::v79071":
+  class { $class2 :
     instance => $instance,
     database => $database,
   }
