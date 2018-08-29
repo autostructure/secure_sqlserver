274806534f50712e79511f1f50aeb697379742da
Frelick <12001003596630@FEDIDCARD.GOV>
2018-08-29 14:54:58 -0400

    fixes

--- manifests/stig/v79071.pp
+++ manifests/stig/v79071.pp
@@ -22,17 +22,21 @@ define secure_sqlserver::stig::v79071 (
       }
     }
 
+    ::secure_sqlserver::log { "v79071: ${database}: is_trustworthy_disabled = ${is_trustworthy_disabled}":
+      loglevel => warning,
+    }
+
     # If the database is MSDB, trustworthy is required to be enabled...
     unless downcase($database) == 'msdb' or $is_trustworthy_disabled {
       $sql_dcl = "ALTER DATABASE ${database} SET TRUSTWORTHY OFF"
 
       ::secure_sqlserver::log { "v79071_sql_dcl = \n${sql_dcl}": }
 
-      sqlserver_tsql{ "v79071_alter_db_disable_trustworthy_${database}":
-        instance => $instance,
-        command  => $sql_dcl,
-        require  => Sqlserver::Config[$instance],
-      }
+      # sqlserver_tsql{ "v79071_alter_db_disable_trustworthy_${database}":
+      #   instance => $instance,
+      #   command  => $sql_dcl,
+      #   require  => Sqlserver::Config[$instance],
+      # }
     }
 
   }
