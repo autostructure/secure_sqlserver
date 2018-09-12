a8b01ce52efdb4f1532ccb220f240895d0a46f32
Frelick <12001003596630@FEDIDCARD.GOV>
2018-09-12 11:27:29 -0400

    added stig

--- manifests/stig/v79075.pp
+++ manifests/stig/v79075.pp
@@ -3,31 +3,32 @@
 # This class manages DISA STIG vulnerability: V-79075
 # SQL Server must limit privileges to change software modules, to include stored procedures, functions, and triggers.
 #
+# $sql = "SELECT P.type_desc AS principal_type, P.name AS principal_name, O.type_desc,
+# CASE class
+# WHEN 0 THEN DB_NAME()
+# WHEN 1 THEN OBJECT_SCHEMA_NAME(major_id) + '.' + OBJECT_NAME(major_id)
+# WHEN 3 THEN SCHEMA_NAME(major_id)
+# ELSE class_desc + '(' + CAST(major_id AS nvarchar) + ')'
+# END AS securable_name, DP.state_desc, DP.permission_name
+# FROM sys.database_permissions DP
+# JOIN sys.database_principals P ON DP.grantee_principal_id = P.principal_id
+# LEFT OUTER JOIN sys.all_objects O ON O.object_id = DP.major_id AND O.type IN ('TR','TA','P','','RF','PC','IF','FN','TF','U')
+# WHERE DP.type IN ('AL','ALTG') AND DP.class IN (0, 1, 53)
+#
+# SELECT R.name AS role_name, M.type_desc AS principal_type, M.name AS principal_name
+# FROM sys.database_principals R
+# JOIN sys.database_role_members DRM ON R.principal_id = DRM.role_principal_id
+# JOIN sys.database_principals M ON DRM.member_principal_id = M.principal_id
+# WHERE R.name IN ('db ddladmin','db_owner')
+# AND M.name != 'dbo'"
+#
 define secure_sqlserver::stig::v79075 (
   Boolean       $enforced = false,
   String[1,16]  $instance = 'MSSQLSERVER',
   String        $database,
 ) {
-  if $enforced {
 
-    # $sql = "SELECT P.type_desc AS principal_type, P.name AS principal_name, O.type_desc,
-    # CASE class
-    # WHEN 0 THEN DB_NAME()
-    # WHEN 1 THEN OBJECT_SCHEMA_NAME(major_id) + '.' + OBJECT_NAME(major_id)
-    # WHEN 3 THEN SCHEMA_NAME(major_id)
-    # ELSE class_desc + '(' + CAST(major_id AS nvarchar) + ')'
-    # END AS securable_name, DP.state_desc, DP.permission_name
-    # FROM sys.database_permissions DP
-    # JOIN sys.database_principals P ON DP.grantee_principal_id = P.principal_id
-    # LEFT OUTER JOIN sys.all_objects O ON O.object_id = DP.major_id AND O.type IN ('TR','TA','P','','RF','PC','IF','FN','TF','U')
-    # WHERE DP.type IN ('AL','ALTG') AND DP.class IN (0, 1, 53)
-    #
-    # SELECT R.name AS role_name, M.type_desc AS principal_type, M.name AS principal_name
-    # FROM sys.database_principals R
-    # JOIN sys.database_role_members DRM ON R.principal_id = DRM.role_principal_id
-    # JOIN sys.database_principals M ON DRM.member_principal_id = M.principal_id
-    # WHERE R.name IN ('db ddladmin','db_owner')
-    # AND M.name != 'dbo'"
+  if $enforced {
 
     ::secure_sqlserver::log { "V-79075: ${instance}\\${database}: sql=${sql}":
       loglevel => notice,
@@ -70,4 +71,5 @@ define secure_sqlserver::stig::v79075 (
     }
 
   }
+
 }
