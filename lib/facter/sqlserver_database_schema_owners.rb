4a515c3028bb53eb7026878d43724f432dd365d3
Frelick <12001003596630@FEDIDCARD.GOV>
2018-09-14 16:25:06 -0400

    starting 79077

---
+++ lib/facter/sqlserver_database_schema_owners.rb
@@ -0,0 +1,29 @@
+# sqlserver_database_schema_owners.rb
+#
+# @return   A hash with the database name and its owner.
+# @example
+#           [ { database => 'master', owner => dbo },
+#             {  database => 'tempdb', owner => dbo },
+#             {  database => 'model', owner => dbo },
+#             {  database => 'msdb', owner => dbo },
+#           ]
+# @dependencies
+#           v79077
+#
+Facter.add('sqlserver_database_schema_owners') do
+  confine operatingsystem: :windows
+  setcode do
+    ret = []
+    sql = "SELECT S.name AS schema_name, P.name AS owning_principal
+FROM sys.schemas S
+JOIN sys.database_principals P ON S.principal_id = P.principal_id
+ORDER BY schema_name"
+    Puppet.debug "sqlserver_database_schema_owners.rb sql...\n#{sql}"
+    client = SqlServerClient.new
+    client.open
+    client.hasharray(sql)
+    databases = client.data
+    client.close unless client.nil? || client.closed?
+    ret
+  end
+end
---
+++ manifests/stig/v79077.pp
@@ -0,0 +1,64 @@
+# This class manages DISA STIG vulnerability: V-79071
+# SQL Server must protect against a user falsely repudiating by ensuring databases
+# are not in a trust relationship.
+#
+# NOTE:
+# If the database is MSDB, trustworthy is required to be enabled and therefore, this is not a finding.
+#
+
+###################################################################################
+# STIG Info...
+###################################################################################
+# Check Text: Obtain a listing of schema ownership from the server documentation.
+#
+# Execute the following query to obtain a current listing of schema ownership.
+#
+# SELECT S.name AS schema_name, P.name AS owning_principal
+# FROM sys.schemas S
+# JOIN sys.database_principals P ON S.principal_id = P.principal_id
+# ORDER BY schema_name
+#
+# If any schema is owned by an unauthorized database principal, this is a finding.
+#
+# Fix Text: Transfer ownership of database schemas to authorized database principals.
+#
+# ALTER AUTHORIZATION ON SCHEMA::[<Schema Name>] TO [<Principal Name>]
+
+define secure_sqlserver::stig::v79071 (
+  Boolean       $enforced = false,
+  String[1,16]  $instance = 'MSSQLSERVER',
+  String        $database,
+) {
+
+  if $enforced {
+
+    $is_trustworthy_enabled = false
+
+    $db_array = $facts['sqlserver_databases_trustworthy_property']
+    $db_array.each |$db_hash| {
+      if downcase($db_hash['database_name']) == downcase($database) {
+        $is_trustworthy_enabled = $db_hash['is_trustworthy_on']
+      }
+    }
+
+    ::secure_sqlserver::log { "v79071: ${database}: is_trustworthy_enabled = ${is_trustworthy_enabled}":
+      loglevel => debug,
+    }
+
+    # If the database is MSDB, trustworthy is required to be enabled...
+    if downcase($database) != 'msdb' and $is_trustworthy_enabled {
+      $sql = "ALTER DATABASE ${database} SET TRUSTWORTHY OFF"
+
+      ::secure_sqlserver::log { "v79071: ${database}: sql = \n${sql}": }
+
+      sqlserver_tsql{ "v79071_alter_db_disable_trustworthy_${instance}_${database}":
+        instance => $instance,
+        database => $database,
+        command  => $sql,
+        require  => Sqlserver::Config[$instance],
+      }
+    }
+
+  }
+
+}
