18d4eb2400172999a07e018d3ec2fa5d770280fa
Frelick <12001003596630@FEDIDCARD.GOV>
2018-09-24 12:05:35 -0400

    added stub class

---
+++ manifests/stig/v79069.pp
@@ -0,0 +1,15 @@
+# v79069.pp
+#
+# SQL Server must protect against a user falsely repudiating by use of system-versioned tables (Temporal Tables).
+#
+define secure_sqlserver::stig::v79069 (
+  Boolean       $enforced = false,
+  String[1,16]  $instance = 'MSSQLSERVER',
+  String        $database,
+) {
+
+  if $enforced {
+    
+  }
+
+}
