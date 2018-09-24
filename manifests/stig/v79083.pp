c93e1020a087062445c79d0719c5b540604564dc
Frelick <12001003596630@FEDIDCARD.GOV>
2018-09-24 12:45:38 -0400

    bug fix

--- manifests/stig/v79083.pp
+++ manifests/stig/v79083.pp
@@ -44,7 +44,7 @@ define secure_sqlserver::stig::v79083 (
 
     $recovery_models = $facts['sqlserver_database_backup_recovery_models']
 
-    unless empty($recovery_models) or empty($recovery_models[$database]) {
+    unless empty($recovery_models) or empty($recovery_models["${database}"]) {
       notify { "v79083: ${instance}\\${database}: recovery_models['${database}'] = ${recovery_models['${database}']}":
         loglevel => notice,
       }
