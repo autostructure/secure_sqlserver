3f2634f4a814382bcc08865bb26cb00f2a2c1300
Frelick <12001003596630@FEDIDCARD.GOV>
2018-09-27 16:21:22 -0400

    sqlserver_temporal_tables v1.0

--- lib/facter/sqlserver_temporal_tables.rb
+++ lib/facter/sqlserver_temporal_tables.rb
@@ -1,24 +1,36 @@
 #!/usr/bin/env ruby -wKU
 # sqlserver_temporal_tables.rb
 #
+# @return array of strings representing a SQL Server temporal table formatted as: SCHEMA_NAME.
+# NOTE: should this be a hash by database.  This fact is used by database level hardening.
+#       maybe it should group tables by database (using a hash instead of an array)
+#
+# SQL from STIG
+# -------------
+# SELECT SCHEMA_NAME(T.schema_id) AS schema_name, T.name AS table_name, T.temporal_type_desc,
+# SCHEMA_NAME(H.schema_id) + '.' + H.name AS history_table
+# FROM sys.tables T
+# JOIN sys.tables H ON T.history_table_id = H.object_id
+# WHERE T.temporal_type != 0
+# ORDER BY schema_name, table_name
+#
 require 'sqlserver_client'
 
 Facter.add('sqlserver_temporal_tables') do
   confine operatingsystem: :windows
   setcode do
 
-    sql = "SELECT SCHEMA_NAME(T.schema_id) AS schema_name, T.name AS table_name, T.temporal_type_desc,
-SCHEMA_NAME(H.schema_id) + '.' + H.name AS history_table
+    sql = "SELECT SCHEMA_NAME(H.schema_id) + '.' + H.name AS history_table
 FROM sys.tables T
 JOIN sys.tables H ON T.history_table_id = H.object_id
 WHERE T.temporal_type != 0
-ORDER BY schema_name, table_name"
+ORDER BY 1"
 
     Puppet.debug "sqlserver_temporal_tables.rb sql...\n#{sql}"
 
     client = SqlServerClient.new
     client.open
-    client.hasharray(sql)
+    client.column(sql)
     resultset = client.data
     client.close unless client.nil? || client.closed?
     resultset
--- manifests/stig/v79069.pp
+++ manifests/stig/v79069.pp
@@ -9,8 +9,44 @@ define secure_sqlserver::stig::v79069 (
 ) {
   if $enforced {
 
-    $temporal_tables = lookup('secure_sqlserver::temporal_tables')
-    $existing_temporals = facts$['sqlserver_temporal_tables']
+    $desired_temporal_tables = lookup('secure_sqlserver::temporal_tables')
+    $existing_temporal_tables = facts$['sqlserver_temporal_tables']
+
+    unless empty($desired_temporal_tables) {
+
+      $desired_temporal_tables.each |$temporal_table| {
+        # check if desired table is already set up...
+        unless empty($existing_temporal_tables) or $temporal_table in $existing_temporal_tables {
+          $sql = "ALTER TABLE ${temporal_table}
+          ADD PERIOD FOR SYSTEM_TIME (SysStartTime, SysEndTime),
+          SysStartTime datetime2 GENERATED ALWAYS AS ROW START HIDDEN NOT NULL DEFAULT GETUTCDATE(),
+          SysEndTime datetime2 GENERATED ALWAYS AS ROW END HIDDEN NOT NULL DEFAULT CONVERT(DATETIME2, '9999-12-31 23:59:59.99999999');"
+
+          ::secure_sqlserver::log { "v79069: calling tsql module for, ${instance}\\${database}, temporal table, ${temporal_table}, using sql = \n${sql}": }
+
+          sqlserver_tsql{ "v79069_make_table_temporal_${temporal_table}_for_${instance}_${database}":
+            instance => $instance,
+            database => $database,
+            command  => $sql,
+            require  => Sqlserver::Config[$instance],
+          }
+
+          # Enable system versioning with 1-year retention for historical data.
+          $sql2 = "ALTER TABLE ${temporal_table} SET (SYSTEM_VERSIONING = ON (HISTORY_RETENTION_PERIOD = 1 YEAR));"
+
+          ::secure_sqlserver::log { "v79069: calling tsql module for, ${instance}\\${database}, temporal table retention set on , ${temporal_table}, using sql = \n${sql2}": }
+
+          sqlserver_tsql{ "v79069_set_table_temporal_retention_${temporal_table}_for_${instance}_${database}":
+            instance => $instance,
+            database => $database,
+            command  => $sql2,
+            require  => Sqlserver::Config[$instance],
+          }
+
+        }
+      }
+
+    }
 
   }
 }
