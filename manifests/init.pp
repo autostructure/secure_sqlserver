c5db86744ce2d74eb7278c402242ee6fd68d9490
Frelick <12001003596630@FEDIDCARD.GOV>
2018-10-18 16:15:28 -0400

    eliminated two params accidentally left in init.pp call

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,21 +24,19 @@ class secure_sqlserver (
   ) {
 
   class { '::secure_sqlserver::controller':
-    approved_shared_accounts                   => $::secure_sqlserver::approved_shared_accounts,
-    approved_sql_login_users                   => $::secure_sqlserver::approved_sql_login_users,
-    audit_filepath                             => $::secure_sqlserver::audit_filepath,
-    audit_maintainer_username                  => $::secure_sqlserver::audit_maintainer_username,
-    backup_plan                                => $::secure_sqlserver::backup_plan,
-    backup_recovery_model_settings             => $::secure_sqlserver::backup_recovery_model_settings,
-    certificate_backup                         => $::secure_sqlserver::certificate_backup,
-    db_master_key_encryption_password          => $::secure_sqlserver::db_master_key_encryption_password,
-    db_master_key_encrypt_backup_file_filepath => $::secure_sqlserver::db_master_key_encrypt_backup_file_filepath,
-    db_master_key_encrypt_backup_file_password => $::secure_sqlserver::db_master_key_encrypt_backup_file_password,
-    new_database_owner                         => $::secure_sqlserver::new_database_owner,
-    schema_owners                              => $::secure_sqlserver::schema_owners,
-    temporal_tables                            => $::secure_sqlserver::temporal_tables,
-    transparent_data_encryption                => $::secure_sqlserver::transparent_data_encryption,
-    port                                       => $::secure_sqlserver::port,
+    approved_shared_accounts          => $::secure_sqlserver::approved_shared_accounts,
+    approved_sql_login_users          => $::secure_sqlserver::approved_sql_login_users,
+    audit_filepath                    => $::secure_sqlserver::audit_filepath,
+    audit_maintainer_username         => $::secure_sqlserver::audit_maintainer_username,
+    backup_plan                       => $::secure_sqlserver::backup_plan,
+    backup_recovery_model_settings    => $::secure_sqlserver::backup_recovery_model_settings,
+    certificate_backup                => $::secure_sqlserver::certificate_backup,
+    db_master_key_encryption_password => $::secure_sqlserver::db_master_key_encryption_password,
+    new_database_owner                => $::secure_sqlserver::new_database_owner,
+    schema_owners                     => $::secure_sqlserver::schema_owners,
+    temporal_tables                   => $::secure_sqlserver::temporal_tables,
+    transparent_data_encryption       => $::secure_sqlserver::transparent_data_encryption,
+    port                              => $::secure_sqlserver::port,
   }
 
 }
